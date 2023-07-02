7d13ee70fafced8b2481f57514cb965bd02ee97d
davidadzavon <106267496+davidadzavon@users.noreply.github.com>
2023-07-02 20:59:11 +0000

    Update app.py

--- app.py
+++ app.py
@@ -103,19 +103,20 @@ folium.LayerControl().add_to(m)
 
 # Average Temperature for the three afro climatic zone 
 
-
 url = "https://raw.githubusercontent.com/davidadzavon/Msc_thesis_webapp/main/MSC_Data_Analysis/Temperature_1/All_temperature.csv"
 response = requests.get(url)
 
 if response.status_code == 200:
     Temp_data = pd.read_csv(url)
 
-    # Now you can use Temp_data for further processing
+    if not Temp_data.empty:
+        Temp_data_na = Temp_data.dropna()
+        # Now you can use Temp_data_na for further processing
+    else:
+        print("DataFrame is empty.")
 else:
     print("Failed to download the CSV file. Status code:", response.status_code)
 
-
-Temp_data_na = Temp_data.dropna()
 fig_temp = px.scatter(Temp_data_na, x="Date", y="Temperature(°C)",              
                 facet_col="Location",  
                 #template = 'plotly_dark',
